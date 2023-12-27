526be69b933c3de6f73589a61cd6faf1fb0f62f9
Shifa Maqsood <shifa_maqsood@hotmail.com>
2023-12-27 03:56:40 -0500

    chrome extension updates

--- index.js
+++ index.js
@@ -1,35 +1,69 @@
 
-let myLeads =[]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputButton = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteButton = document.getElementById("delete-btn")
+const leadsFromStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
 
-inputButton.addEventListener("click", function(){
-myLeads.push(inputEl.value)
-// inputEL.value = ""
-renderLeads()
+
+if (leadsFromStorage) {
+    myLeads = leadsFromStorage
+    render(myLeads)
+}
+
+
+tabBtn.addEventListener("click", function(){
+        
+        chrome.tabs.query({active: true, currentWindow: true}, function(tab){
+            myLeads.push(tabs[0].url)
+
+localStorage.setItem("myLeads", JSON.stringify(myLeads))
+render(myLeads)
+
+        })   
+
 })
 
-function renderLeads (){
-    let listIems = " "
+function render(leads){
+    let listIems = ""
 
-    for( let i = 0; i < myLeads.length; i++){
-   //listIems += "<li><a target = '_blank' href = '" + myLeads[i]+ "' > " +  myLeads[i]+ "</a></li>"
+    for( let i = 0; i < leads.length; i++){
+   //listIems += "<li><a target = '_blank' href = '" + leads[i]+ "' > " +  myLeads[i]+ "</a></li>"
 listIems += `
 <li>
-        <a target = '_blank' href = '${myLeads[i]}'>
-        ${myLeads[i]}
+        <a target = '_blank' href = '${leads[i]}'>
+        ${leads[i]}
         </a>
 </li>
 
 `
-
     }
     ulEl.innerHTML =  listIems 
-    let input = document.getElementById("input-el")
-input.value = "";
-
+   
     }
+
+deleteButton.addEventListener('dblclick',function(){
+    localStorage.clear()
+    myLeads=[]
+    render(myLeads)
+
+})
+
+
+
+
+
+inputButton.addEventListener("click", function(){
+myLeads.push(inputEl.value)
+inputEl.value = ""
+localStorage.setItem("myLeads", JSON.stringify(myLeads))
+render(myLeads)
+
+})
+
+
 
 
 
